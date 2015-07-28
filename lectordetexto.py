15dfee029ecc4413da7d140410b1104e8a6179fe
radioGIS <radiogis@radiogis.uis.edu.co>
2015-07-28 16:42:46 -0500

    Borrar comentarios

--- lectordetexto.py
+++ lectordetexto.py
@@ -14,23 +14,17 @@ def main():
      for linea in archivo:
        print(linea)
        cadenadedatos+=linea[0:-1]+";"
-       #sel=linea.split()
-      # n=0
-       #for valor in sel:
-        # print(valor)
-         #n=n+1
-         #coment="sensor "+str(n) 
-         #date=time.strftime('%d/%m/%Y %H:%M:%S')
+       
      cadenadedatos=cadenadedatos[0:-1]
      print(cadenadedatos)
     
      print(date)
      sendRaw(date, 1, cadenadedatos, 2, 3, -73.45, 7.87, 3)
 
-         #sendRaw(date, 1, valor, 2, 3, -73.45, 7.87,coment)
+         
      os.remove(datosarenas)               
                 
-            #print(sel)
+            
 
 
 main()
